0cde49883d876f022cd58b57bc58cad9cade8fb9
raviteja <raviteja.gadige@time.com.my>
2020-03-22 15:57:01 +0800

    pod updates

---
+++ Pods/Target Support Files/ApptivityFramework/ApptivityFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ApptivityFramework : NSObject
+@end
+@implementation PodsDummy_ApptivityFramework
+@end
---
+++ Pods/Target Support Files/Pods-TimeSelfCareData/Pods-TimeSelfCareData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TimeSelfCareData : NSObject
+@end
+@implementation PodsDummy_Pods_TimeSelfCareData
+@end

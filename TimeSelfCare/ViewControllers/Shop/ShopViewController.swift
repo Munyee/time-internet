f5ec0b1bade059daba9f50a91e87d8b8e35c9e06
raviteja <raviteja.gadige@time.com.my>
2020-09-11 14:45:29 +0800

    code refactor

--- TimeSelfCare/ViewControllers/Shop/ShopViewController.swift
+++ TimeSelfCare/ViewControllers/Shop/ShopViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ShopViewController: TimeBaseViewController, WKUIDelegate {
     
+    @IBOutlet private var webViewContainer: UIView?
     var webView: WKWebView! // swiftlint:disable:this implicitly_unwrapped_optional
     var parameters: [String: Any] = [:]
     
@@ -18,6 +19,24 @@ class ShopViewController: TimeBaseViewController, WKUIDelegate {
         super.viewDidLoad()
         self.title = NSLocalizedString("Shop", comment: "")
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_back_arrow"), style: .done, target: self, action: #selector(self.dismissVC(_:)))
+        
+        if let webViewContainer = webViewContainer {
+            // Initialize:
+            let webView = WKWebView(frame: webViewContainer.bounds, configuration: WKWebViewConfiguration()) // Create a new web view
+            webView.translatesAutoresizingMaskIntoConstraints = false // This needs to be called due to manually adding constraints
+
+            // Add as a subview:
+            webViewContainer.addSubview(webView)
+
+            // Add constraints to be the same as webViewContainer
+            webViewContainer.addConstraint(NSLayoutConstraint(item: webView, attribute: .leading, relatedBy: .equal, toItem: webViewContainer, attribute: .leading, multiplier: 1.0, constant: 0.0))
+            webViewContainer.addConstraint(NSLayoutConstraint(item: webView, attribute: .trailing, relatedBy: .equal, toItem: webViewContainer, attribute: .trailing, multiplier: 1.0, constant: 0.0))
+            webViewContainer.addConstraint(NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: webViewContainer, attribute: .top, multiplier: 1.0, constant: 0.0))
+            webViewContainer.addConstraint(NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: webViewContainer, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+
+            // Assign web view for reference
+            self.webView = webView
+        }
         
         let path = "open_shop"
         parameters["action"] = path
@@ -27,29 +46,19 @@ class ShopViewController: TimeBaseViewController, WKUIDelegate {
         parameters["bulb"] = "yes"
         parameters["session_id"] = AccountController.shared.sessionId
         
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
-        self.view = webView
-
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: NSLocalizedString("Cancel", comment: ""), style: .plain, target: self, action: #selector(self.webViewDidClose(_:)))
-        
         var request = URLRequest(url: APIClient.BaseAPIURL)
         request.httpMethod = "POST"
         request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         webView.load(request)
-        
     }
 }
 
 extension ShopViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) { // swiftlint:disable:this implicitly_unwrapped_optional
-        debugPrint("Commit... \(webView.url)")
-//        guard let url = webView.url else {
-//            return
-//        }
+        guard let url = webView.url else {
+            return }
+        debugPrint("Commit... \(url)")
     }
 
     func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) { // swiftlint:disable:this implicitly_unwrapped_optional
@@ -58,6 +67,12 @@ extension ShopViewController: WKNavigationDelegate {
 
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) { // swiftlint:disable:this implicitly_unwrapped_optional
         debugPrint("Start...")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) { // swiftlint:disable:this implicitly_unwrapped_optional
+        webView.evaluateJavaScript("document.title") { (string, error) in
+            print("title = \(String(describing: string))")
+        }
     }
 
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) { // swiftlint:disable:this implicitly_unwrapped_optional

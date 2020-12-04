54577f58bcb7bc3740a6d45ba02703c04c796091
raviteja <raviteja.gadige@time.com.my>
2020-12-04 18:58:46 +0800

    add icons

--- TimeSelfCare/ViewControllers/Reward/RewardViewController.swift
+++ TimeSelfCare/ViewControllers/Reward/RewardViewController.swift
@@ -110,7 +110,7 @@ class RewardViewController: TimeBaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = NSLocalizedString("TIME Rewards", comment: "")
+        self.title = NSLocalizedString("TIME REWARDS", comment: "")
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_back_arrow"), style: .done, target: self, action: #selector(self.dismissVC(_:)))
 
         self.tableView.addSubview(self.refreshControl)

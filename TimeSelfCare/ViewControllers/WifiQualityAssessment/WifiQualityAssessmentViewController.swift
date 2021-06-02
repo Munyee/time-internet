65acff8de5458f9bedafbb553fcf52d58baf8fd6
Chan Mun Yee <yee_chan22@msn.com>
2021-06-02 10:43:56 +0800

    Added view controller

---
+++ TimeSelfCare/ViewControllers/WifiQualityAssessment/WifiQualityAssessmentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WifiQualityAssessmentViewController.swift
+//  TimeSelfCare
+//
+//  Created by Chan Mun Yee on 02/06/2021.
+//  Copyright © 2021 Apptivity Lab. All rights reserved.
+//
+
+import UIKit
+
+class WifiQualityAssessmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
